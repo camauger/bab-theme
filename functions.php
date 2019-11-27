d88f3ab6ab9218c5c1c4b571de9a95960c0f06fd
Christian Amauger <christian.amauger@gmail.com>
2019-11-27 13:53:52 -0500

    functions update

--- functions.php
+++ functions.php
@@ -255,29 +255,45 @@ function collectionCatEn($catName, $catSlug, $imgSlug)
 //**
 // Christmas Products (Kits)
 // */
-
 function kit($lang, $kit, $items)
 {
-	$kithtml = '';
-	$kitName = '';
-	if ($lang == 'en') :
-		$kitName = esc_html($kit[0]);
-	elseif ($lang == 'fr') :
-		$kitName = esc_html($kit[0]);
-	endif;
+    // Déclarations des variables
+    $kithtml = '';
+    $kitName = '';
+    $kitImage = esc_html($kit[1]);
+    $kitUrl = esc_html($kit[2]);
 
-	$kithtml .= '<div class="ball--first ball ball--big ball--empty">
-    <a href="' . esc_html($kit[2]) . '"><img src="' . esc_html($kit[1]) . '" alt=""></a>
+    // Déclarations des variables en fonction de la langue
+    if ($lang == 'en') :
+        $kitName = esc_html($kit[0]);
+    elseif ($lang == 'fr') :
+        $kitName = esc_html($kit[0]);
+    endif;
+    // Le Kit
+    $kithtml = '<div class="ball--first ball ball--big ball--empty">
+    <a href="' . $kitUrl . '"><img src="' . $kitImage . '" alt="' . $kitName . '"></a>
     <a class="kit__name kit--big" href="">' . $kitName . '</a><span class="ball__shadow"></span>
 </div>';
+    // Les items appartenant au kit
+    foreach ($items as $item) {
+        // Déclarations des variables en fonction de la langue
+        //  1 - itemName FR, 2 - itemName EN, 3 - itemImage, 4 - itemURl FR, 5 - itemUrl EN
+        $itemImage = esc_html($item[2]);
+        $itemUrl = '';
+        $itemName = '';
+        if ($lang == 'en') :
+            $itemName = esc_html($item[1]);
+            $itemUrl = esc_html($item[4]);
+        elseif ($lang == 'fr') :
+            $itemName = esc_html($item[0]);
+            $itemUrl = esc_html($item[3]);
+        endif;
 
-
-
-	foreach ($items as $item) {
-		$kithtml .= '<div class="ball ball--small ball--empty ball--one">
-    <a href="' . esc_html($item[2]) . '"><img src="' . esc_html($item[1]) . '" alt="' . esc_html($item[0]) . '"></a>
-    <a class="kit__name kit--one" href="">' . esc_html($item[0]) . '</a><span class="ball__shadow"></span>
+        $kithtml .= '<div class="ball ball--small ball--empty ball--one">
+    <a href="' . $itemUrl . '"><img src="' . $itemImage . '" alt="' . $itemName . '"></a>
+    <a class="kit__name kit--one" href="' . $itemUrl . '">' . $itemName . '</a><span class="ball__shadow"></span>
 </div>';
-	}
-	echo $kithtml;
+    }
+    // Affichage de l'ensemble
+    echo $kithtml;
 };
