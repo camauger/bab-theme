29244de7b50881fdd7e7d90a7b204b7c2feae81f
Christian Amauger <christian.amauger@gmail.com>
2019-12-02 14:40:48 -0500

    new products h3

--- page-templates/home.php
+++ page-templates/home.php
@@ -109,7 +109,7 @@ $container = get_theme_mod('understrap_container_type');
 		</div>
 	</div>
 	<div>
-		<h3><?php esc_html_e('New Items', 'bab'); ?></h3>
+		<h3 class="new-products__title"><?php esc_html_e('New Items', 'bab'); ?></h3>
 		<?php echo do_shortcode('[products limit="4" columns="4" orderby="id" order="DESC" visibility="visible"]') ; ?>
 	</div>
 	<!-- présentation services -->
