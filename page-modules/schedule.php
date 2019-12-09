238325c0ff02e5dea0237877995dc2bea24a18ba
Christian Amauger <christian.amauger@gmail.com>
2019-12-09 09:49:59 -0500

    schedule update

--- page-modules/schedule.php
+++ page-modules/schedule.php
@@ -1,9 +1,13 @@
-<strong><?php esc_html_e('Monday, Tuesday', 'bab'); ?></strong>
+<strong><?php esc_html_e('Monday', 'bab'); ?></strong>
 <span><?php echo esc_html($post->monday) ?></span>
+<strong><?php esc_html_e('Tuesday', 'bab'); ?></strong>
+<span><?php echo esc_html($post->tuesday) ?></span>
 <strong><?php esc_html_e('Wednesday', 'bab'); ?></strong>
 <span><?php echo esc_html($post->wednesday) ?></span>
-<strong><?php esc_html_e('Thursday, Friday', 'bab'); ?></strong>
+<strong><?php esc_html_e('Thursday', 'bab'); ?></strong>
 <span><?php echo esc_html($post->thursday) ?></span>
+<strong><?php esc_html_e('Friday', 'bab'); ?></strong>
+<span><?php echo esc_html($post->friday) ?></span>
 <strong><?php esc_html_e('Saturday', 'bab'); ?></strong>
 <span><?php echo esc_html($post->saturday) ?></span>
 <strong><?php esc_html_e('Sunday', 'bab'); ?></strong>
