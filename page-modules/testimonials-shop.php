28510e0a2aec752c979c0b96069f96d8e3a21fff
Christian Amauger <christian.amauger@gmail.com>
2020-03-02 11:50:53 -0500

    testimonials name fix

--- page-modules/testimonials-shop.php
+++ page-modules/testimonials-shop.php
@@ -1,9 +1,9 @@
 <blockquote>
     <?php esc_html_e('A thought full of gratitude inspired by the generosity and kindness that you show to us. Meeting an exceptional person makes all the difference in our lives. It is a real pleasure to know you, Ms. Rouleau.', 'bab'); ?>
-    <span>Mme Huguette Gagné</span>
+    <span>Mme H. Gagné</span>
 </blockquote>
 
 <blockquote>
     <?php esc_html_e('I received my jewelry on February 12, a beautiful old ring with garnets and pearls, it is really beautiful and has been well preserved over time. Garnet is my birthstone. It was my 3rd purchase and I am really happy with my purchases. Thank you for the excellent service, the jewelry is carefully packaged. I look at your online store very often!', 'bab'); ?>
-    <span>Mme Manon Aubé</span>
+    <span>Mme M. Aubé</span>
 </blockquote>
