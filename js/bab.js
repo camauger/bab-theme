6caf05356eefc3ce9f092dd9ae385ebc50026de5
Christian Amauger <christian.amauger@gmail.com>
2020-03-03 14:43:22 -0500

    broches page h1 styling

--- js/bab.js
+++ js/bab.js
@@ -1,9 +1,16 @@
 // Category page
-let catPage = document.querySelector("body.tax-product_cat");
+const catPage = document.querySelector("body.tax-product_cat");
 if (catPage) {
 	console.log("This is a category page.");
 }
 
 if (document.body.classList.contains("tax-product_cat")) {
 	console.log("This is indeed a category page.");
+}
+
+const brochesPage = document.querySelector("body.term-broches-bp");
+
+if (brochesPage) {
+	const h1 = document.querySelector("h1");
+	h1.style.fontFamily = "Qwigley,cursive";
 }
